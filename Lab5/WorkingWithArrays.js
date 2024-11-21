5035cf1e20dfb20bf221fff6c5b59a1778bafed5
cuanfraser <33520989+cuanfraser@users.noreply.github.com>
2024-11-21 17:11:55 -0500

    A5 3.6 - 3.6.3

--- Lab5/WorkingWithArrays.js
+++ Lab5/WorkingWithArrays.js
@@ -15,6 +15,12 @@ export default function WorkingWithArrays(app) {
         res.json(todos);
     });
 
+    app.post('/lab5/todos', (req, res) => {
+        const newTodo = { ...req.body, id: new Date().getTime() };
+        todos.push(newTodo);
+        res.json(newTodo);
+    });
+
     app.get('/lab5/todos', (req, res) => {
         const { completed } = req.query;
         if (completed !== undefined) {
@@ -39,6 +45,13 @@ export default function WorkingWithArrays(app) {
         res.json(todos);
     });
 
+    app.delete('/lab5/todos/:id', (req, res) => {
+        const { id } = req.params;
+        const todoIndex = todos.findIndex((t) => t.id === parseInt(id));
+        todos.splice(todoIndex, 1);
+        res.sendStatus(200);
+    });
+
     app.get('/lab5/todos/:id/title/:title', (req, res) => {
         const { id, title } = req.params;
         const todo = todos.find((t) => t.id === parseInt(id));
@@ -58,5 +71,16 @@ export default function WorkingWithArrays(app) {
         const todo = todos.find((t) => t.id === parseInt(id));
         todo.description = description;
         res.json(todos);
+    });
+
+    app.put('/lab5/todos/:id', (req, res) => {
+        const { id } = req.params;
+        todos = todos.map((t) => {
+            if (t.id === parseInt(id)) {
+                return { ...t, ...req.body };
+            }
+            return t;
+        });
+        res.sendStatus(200);
     });
 }
