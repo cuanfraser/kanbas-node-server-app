fdc175340ed429be7aa793466765522d31744bb3
cuanfraser <33520989+cuanfraser@users.noreply.github.com>
2024-11-22 19:13:59 -0500

    Fix enrollUserInCourse not returning newEnrollment

--- Kanbas/Enrollments/dao.js
+++ Kanbas/Enrollments/dao.js
@@ -2,7 +2,9 @@ import Database from '../Database/index.js';
 
 export function enrollUserInCourse(userId, courseId) {
     const { enrollments } = Database;
-    enrollments.push({ _id: Date.now(), user: userId, course: courseId });
+    const newEnrollment = { _id: Date.now(), user: userId, course: courseId };
+    enrollments.push(newEnrollment);
+    return newEnrollment;
 }
 
 export function findEnrollmentsForUser(userId) {
