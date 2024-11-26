9ba346eae72126e0ee513644ddad504d41d57e3c
cuanfraser <33520989+cuanfraser@users.noreply.github.com>
2024-11-26 18:12:25 -0500

    A6 3.6.4

--- Kanbas/Users/routes.js
+++ Kanbas/Users/routes.js
@@ -28,7 +28,10 @@ export default function UserRoutes(app) {
         const users = await dao.findAllUsers();
         res.json(users);
     };
-    const findUserById = (req, res) => {};
+    const findUserById = async (req, res) => {
+        const user = await dao.findUserById(req.params.userId);
+        res.json(user);
+    };
     const updateUser = async (req, res) => {
         const userId = req.params.userId;
         const userUpdates = req.body;
