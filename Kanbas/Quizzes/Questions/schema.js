32df8df53917821e0fc2b4a0328c24e7d3d58021
cuanfraser <33520989+cuanfraser@users.noreply.github.com>
2024-12-02 18:25:06 -0500

    Make question type required

--- Kanbas/Quizzes/Questions/schema.js
+++ Kanbas/Quizzes/Questions/schema.js
@@ -1,14 +1,15 @@
 import mongoose from 'mongoose';
 const questionSchema = new mongoose.Schema(
   {
-    quiz_id: { type: mongoose.Schema.Types.ObjectId, ref: 'QuizzesModel' },
-    title: String,
-    points: Number,
+    quiz_id: { type: mongoose.Schema.Types.ObjectId, ref: 'QuizzesModel', required: true },
+    title: { type: String, required: true },
+    points: { type: Number, default: 1, required: true },
     question: String,
     type: {
       type: String,
       enum: ['MULTIPLE_CHOICE', 'TRUE_FALSE', 'FILL_IN'],
       default: 'MULTIPLE_CHOICE',
+      required: true,
     },
     choices: [String],
     answer: String,
