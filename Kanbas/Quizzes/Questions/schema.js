204e4f41ddce8fcfb6ec7e32828beacc33156bb8
cuanfraser <33520989+cuanfraser@users.noreply.github.com>
2024-12-03 19:34:57 -0500

    Remove question schema requireds

--- Kanbas/Quizzes/Questions/schema.js
+++ Kanbas/Quizzes/Questions/schema.js
@@ -2,9 +2,9 @@ import mongoose from 'mongoose';
 const questionSchema = new mongoose.Schema(
   {
     quiz_id: { type: mongoose.Schema.Types.ObjectId, ref: 'QuizzesModel', required: true },
-    title: { type: String, required: true },
+    title: String,
     points: { type: Number, default: 1, required: true },
-    question: { type: String, default: '', required: true },
+    question: String,
     type: {
       type: String,
       enum: ['MULTIPLE_CHOICE', 'TRUE_FALSE', 'FILL_IN'],
@@ -16,7 +16,7 @@ const questionSchema = new mongoose.Schema(
       default: [],
       required: true,
     },
-    answer: { type: String, default: '', required: true },
+    answer: String,
   },
   { collection: 'questions' }
 );
